1ef07d18999d048b566a32d8f4408a1bd396260e
René Bannat <r.bannat@gmail.com>
2020-02-09 17:02:29 +0100

    update source plugin and add player scores for first and second half

--- src/components/team-page.js
+++ src/components/team-page.js
@@ -1,10 +1,10 @@
-import React from "react"
+import React, { useState } from "react"
 import { graphql } from "gatsby"
 import Layout from "./layout"
 import SEO from "./seo"
 
 const TeamPage = ({ data }) => {
-  const players = data.allPlayer.edges
+  const [activeTab, setActiveTab] = useState("firstHalf")
   return (
     <Layout>
       <SEO title="Team" />
@@ -17,25 +17,59 @@ const TeamPage = ({ data }) => {
       </div>
       <section className="section">
         <div className="container">
+          <div className="tabs">
+            <ul>
+              <li className={activeTab === "firstHalf" ? "is-active" : ""}>
+                <a onClick={() => setActiveTab("firstHalf")}>Hinrunde</a>
+              </li>
+              <li className={activeTab === "secondHalf" ? "is-active" : ""}>
+                <a onClick={() => setActiveTab("secondHalf")}>Rückrunde</a>
+              </li>
+            </ul>
+          </div>
+
           <div className="table-container">
             <table className="table is-fullwidth">
               <thead>
                 <tr>
+                  <th>Pos</th>
                   <th>Name</th>
+                  <th>Sp</th>
+                  <th>S</th>
+                  <th>N</th>
                   <th>Score</th>
                 </tr>
               </thead>
               <tbody>
-                {players.map(({ node: { name, score, id } }) => {
-                  return (
-                    <tr key={id}>
-                      <td>
-                        {name}
-                      </td>
-                      <td>{score}</td>
-                    </tr>
-                  )
-                })}
+                {(activeTab === "firstHalf"
+                  ? data.team.playersFirstHalf
+                  : data.team.playersSecondHalf
+                ).map(
+                  ({
+                    player: {
+                      id,
+                      name,
+                      playerScoresFirstHalf,
+                      playerScoresSecondHalf,
+                    },
+                    position,
+                  }) => {
+                    const { score, won, lost, gamesPlayed } =
+                      activeTab === "firstHalf"
+                        ? playerScoresFirstHalf
+                        : playerScoresSecondHalf
+                    return (
+                      <tr key={id}>
+                        <td>{position}</td>
+                        <td>{name}</td>
+                        <td>{gamesPlayed}</td>
+                        <td>{won}</td>
+                        <td>{lost}</td>
+                        <td>{score}</td>
+                      </tr>
+                    )
+                  }
+                )}
               </tbody>
             </table>
           </div>
@@ -49,13 +83,30 @@ export const query = graphql`
   query TeamPageQuery($teamId: String!) {
     team(id: { eq: $teamId }) {
       name
-    }
-    allPlayer(sort: {fields: position}, filter: {team: {id: {eq: $teamId}}}) {
-      edges {
-        node {
+      playersFirstHalf {
+        position
+        player {
           id
           name
-          score
+          playerScoresFirstHalf {
+            score
+            won
+            lost
+            gamesPlayed
+          }
+        }
+      }
+      playersSecondHalf {
+        position
+        player {
+          id
+          name
+          playerScoresSecondHalf {
+            score
+            won
+            lost
+            gamesPlayed
+          }
         }
       }
     }
